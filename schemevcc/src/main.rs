04f9e56e1c464d436d2d7a15e82983c77f31b2a3
charlene-j <148769967+charlene-j@users.noreply.github.com>
2024-02-24 23:41:39 +0100

    Update4 main.rs

--- schemevcc/src/main.rs
+++ schemevcc/src/main.rs
@@ -8,9 +8,9 @@ fn main(){
     let mut csprng = OsRng;	
     let n = 108;
     let d = 5; // number of players
-    let genplayers = vec![g0;d];
+    let genplayers = vec![g0; d];
     let genmaster = g0;
-    let clueforms = vec![0,0,1,1,1]; // two different forms of clue: 0 and 1.
+    let clueforms = vec![0, 0, 1, 1, 1]; // two different forms of clue: 0 and 1.
     
     let iter = 10;
     measuresvcc(&mut csprng, genmaster, genplayers, clueforms, n, iter);  				
