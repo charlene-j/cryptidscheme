61bac8d9eb2cb0728d55e5fa47dbbc1cc97ea12e
charlene-j <148769967+charlene-j@users.noreply.github.com>
2024-02-24 23:47:25 +0100

    Update2 main.rs

--- scheme2/src/main.rs
+++ scheme2/src/main.rs
@@ -5,7 +5,7 @@ use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
 fn main(){  
     let mut csprng = OsRng;	
     let g0 = RISTRETTO_BASEPOINT_POINT; // g0 is the generator used for the public key of the player.
-    println!("Performance measurements:"); //Test of performances on iter iterations 
+    println!("Performance measurements:"); // Test of performances on iter iterations 
     let iter = 10;
     measurementscc2(&mut csprng, g0, iter);					
 }
