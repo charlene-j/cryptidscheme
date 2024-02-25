886290c3915981975346d50b200b2be296b6f890
charlene-j <148769967+charlene-j@users.noreply.github.com>
2024-02-25 16:35:49 +0100

    Update main.rs

--- scheme1/src/main.rs
+++ scheme1/src/main.rs
@@ -1,36 +1,14 @@
-use cryptocryptid::{buildrandomclue,schemecc1,mesuretimeandsize,funanswer};
-use curve25519_dalek::{ristretto::RistrettoPoint,constants::RISTRETTO_BASEPOINT_POINT,traits::Identity}; 
 use rand_core::OsRng;
-use rand::Rng;
+use curve25519_dalek::{ristretto::RistrettoPoint, constants::RISTRETTO_BASEPOINT_POINT, traits::Identity}; 
+use cryptocryptid::measurementscc1;
    
 fn main(){
 
-    let g0 = RISTRETTO_BASEPOINT_POINT;
     let mut csprng = OsRng; 
-    let a = vec![RistrettoPoint::identity(),RISTRETTO_BASEPOINT_POINT];
-    
-    // Generates a random clue
-    let clue = buildrandomclue((&a).to_vec());
-    
-    // Generates a random index of cell
-    let mut rng = rand::thread_rng();
-    let j = rng.gen_range(0..108);
-
-    // Generates the answer corresponding to the clue and the cell j
-    let answer = funanswer((&a).to_vec(),(&clue).to_vec(),j);
-    
-    /*let mut badanswer = 2;
-    if answer == 0{
-    	badanswer = 1;
-    }
-    else{
-    	badanswer = 0;
-    }*/
-    
-    // Runs the alogrithms for the protocol cc1 
-    schemecc1(& mut csprng,g0,(&a).to_vec(),(&clue).to_vec(),j,answer);
-    
-    // Calculates a mean of the execution time of the genclue, openclue, play and verify algorithms over a number of iterations
-    let iter = 500;
-    mesuretimeandsize(& mut csprng,g0,(&a).to_vec(),iter);     
+    let g0 = RISTRETTO_BASEPOINT_POINT; // g0 is the generator used to build the public key of a player
+    let a = vec![RistrettoPoint::identity(), RISTRETTO_BASEPOINT_POINT]; // a is the encoding vector of true and false in RistrettoPoint.
+    let n = 108;
+    println!("Performance measurements:"); // Test of performances on iter iterations.    
+    let iter = 10;
+    measurementscc1(&mut csprng, g0, (&a).to_vec(), n, iter);     
 } 
