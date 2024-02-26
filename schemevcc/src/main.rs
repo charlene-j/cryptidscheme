fd3a3f71513fcd95bf8d9327c48e533aead3be64
charlene-j <148769967+charlene-j@users.noreply.github.com>
2024-02-26 23:20:16 +0100

    Update main.rs

--- schemevcc/src/main.rs
+++ schemevcc/src/main.rs
@@ -1,16 +1,17 @@
-use cryptocryptid::measuresvcc;
+use rand_core::OsRng;
 use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
-use rand_core::{OsRng};
+use cryptocryptid::measurementsvcc;
 
 fn main(){
     
-    let g0 = RISTRETTO_BASEPOINT_POINT; // g0 is the generator used for the public key of the game master.
-    let mut csprng = OsRng;	
+    let mut csprng = OsRng;
+    let g0 = RISTRETTO_BASEPOINT_POINT; // g0 is the generator used to build the public key of the game master.	
     let n = 108;
-    let d = 5; // number of players
+    let d = 5; // d is the number of players.
     let genplayers = vec![g0; d];
     let genmaster = g0;
-    let clueforms = vec![0, 0, 1, 1, 1]; // two different forms of clue: 0 and 1.
-    let iter = 10; // Average over iter iterations.
-    measuresvcc(&mut csprng, genmaster, genplayers, clueforms, n, iter);  				
+    let clueforms = vec![0, 0, 1, 1, 1]; // There are two different forms of clue: (Tj, Tk, bottom) or (bottom, bottom, P) where P in Pj.
+    println!("Performance measurements for {:?} players:", d); // Test of performances on iter iterations. 
+    let iter = 10;
+    measurementsvcc(&mut csprng, genmaster, genplayers, clueforms, n, iter);  				
 }
