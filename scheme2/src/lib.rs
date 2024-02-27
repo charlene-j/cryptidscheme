50bfef86af3f4070ffc171823ab0673ad45a32a3
charlene-j <148769967+charlene-j@users.noreply.github.com>
2024-02-27 10:15:55 +0100

    Update lib.rs

--- scheme2/src/lib.rs
+++ scheme2/src/lib.rs
@@ -20,48 +20,7 @@ mod tests{
         let p = c.1; // Vector of fourteen properties.
         let bottom = c.2; 
 	
-        // It defines a random cell
-        let cj = randomcell((&t).to_vec(), (&p).to_vec(), 13); // 13 is the number of properties of the cell.
-        let tj = cj.0;
-        let pj = cj.1;
-    
-        for i in 0..4{
-
-            let clue = buildclue((&t).to_vec(), (&p).to_vec(), bottom, tj, (&pj).to_vec(), i);
-            let keyc = genclue(&mut csprng, g0, (&clue).to_vec());               
-            let pc = keyc.0; //pc = (pk, c11, c21, c12, c22, c13, c23)
-            let sc = keyc.1; //sc = sk
-
-            let open = openclue((&pc).to_vec(), sc);
-            assert!(open == (&clue).to_vec(), "Open clue is not equal to clue");
-            
-            let answer = algoanswer(bottom, (&clue).to_vec(), tj, (&pj).to_vec());
-            
-            if i == 0 || i == 1{
-                assert!(answer == 1, "Bad answer");
-            }
-            else{
-                assert!(answer == 0, "Bad answer");
-            }
-            
-            let proof = play(&mut csprng, g0, (&pc).to_vec(), sc, tj, (&pj).to_vec(), answer);
-            let b = verify(g0, proof, (&pc).to_vec(), tj, (&pj).to_vec(), answer);
-            assert!(b == true, "Verify is false");	
-        }
-    }
-    #[test] // 
-    // Same as testzkp1 except that it tests if the player give a bad answer implies that the algorithm Verify returns false.
-    fn test2cc2(){ 
-
-        // Generation of properties and map and cell.
-        let mut csprng = OsRng;	
-        let g0 = RISTRETTO_BASEPOINT_POINT; // g0 is the generator used in the public key of the player.
-        let c = typesandproperties(&mut csprng); // Random generation of five types and fourteen properties in RistrettoPoint.
-        let t = c.0; // Vector of five types.
-        let p = c.1; // Vector of fourteen properties.
-        let bottom = c.2; 
-	
-        // It defines a random cell
+        // It defines a random cell.
         let cj = randomcell((&t).to_vec(), (&p).to_vec(), 13); // 13 is the number of properties of the cell.
         let tj = cj.0;
         let pj = cj.1;
@@ -74,7 +33,48 @@ mod tests{
             let sc = keyc.1; // sc = sk.
 
             let open = openclue((&pc).to_vec(), sc);
-            assert!(open == (&clue).to_vec(), "The open clue is not equal to clue");
+            assert!(open == (&clue).to_vec(), "The open clue is not equal to the clue.");
+            
+            let answer = algoanswer(bottom, (&clue).to_vec(), tj, (&pj).to_vec());
+            
+            if i == 0 || i == 1{
+                assert!(answer == 1, "Incorrect answer");
+            }
+            else{
+                assert!(answer == 0, "Incorrect answer");
+            }
+            
+            let proof = play(&mut csprng, g0, (&pc).to_vec(), sc, tj, (&pj).to_vec(), answer);
+            let b = verify(g0, proof, (&pc).to_vec(), tj, (&pj).to_vec(), answer);
+            assert!(b == true, "Verify is false.");	
+        }
+    }
+    #[test] // 
+    // Same as test1cc2 except that it tests if the player give a bad answer implies that the algorithm Verify returns false.
+    fn test2cc2(){ 
+
+        // Generation of properties and map and cell.
+        let mut csprng = OsRng;	
+        let g0 = RISTRETTO_BASEPOINT_POINT; // g0 is the generator used in the public key of the player.
+        let c = typesandproperties(&mut csprng); // Random generation of five types and fourteen properties in RistrettoPoint.
+        let t = c.0; // Vector of five types.
+        let p = c.1; // Vector of fourteen properties.
+        let bottom = c.2; 
+	
+        // It defines a random cell.
+        let cj = randomcell((&t).to_vec(), (&p).to_vec(), 13); // 13 is the number of properties of the cell.
+        let tj = cj.0;
+        let pj = cj.1;
+    
+        for i in 0..4{
+
+            let clue = buildclue((&t).to_vec(), (&p).to_vec(), bottom, tj, (&pj).to_vec(), i);
+            let keyc = genclue(&mut csprng, g0, (&clue).to_vec());               
+            let pc = keyc.0; // pc = (pk, c11, c21, c12, c22, c13, c23).
+            let sc = keyc.1; // sc = sk.
+
+            let open = openclue((&pc).to_vec(), sc);
+            assert!(open == (&clue).to_vec(), "The open clue is not equal to the clue.");
             
             let answer = algoanswer(bottom, (&clue).to_vec(), tj, (&pj).to_vec());
             let mut badanswer = 2;
@@ -88,7 +88,9 @@ mod tests{
              
             let proof = play(&mut csprng, g0, (&pc).to_vec(), sc, tj, (&pj).to_vec(), badanswer);
             let b = verify(g0, proof, (&pc).to_vec(), tj, (&pj).to_vec(), badanswer);
-            assert!(b == false, "Verify is true");
+            assert!(b == false, "Verify is true.");
         }
+    }
+}
     }
 }
