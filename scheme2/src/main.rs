f2b0907d2e4d4f447e2ca8ace79324ab595b45f3
charlene-j <148769967+charlene-j@users.noreply.github.com>
2024-02-27 10:15:28 +0100

    Update main.rs

--- scheme2/src/main.rs
+++ scheme2/src/main.rs
@@ -1,11 +1,12 @@
 use rand_core::OsRng;
-use cryptocryptid::measurementscc2;
 use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
+use cryptocryptid::measurementscc2;
 
 fn main(){  
+
     let mut csprng = OsRng;	
     let g0 = RISTRETTO_BASEPOINT_POINT; // g0 is the generator used for the public key of the player.
-    println!("Performance measurements:"); // Test of performances on iter iterations 
-    let iter = 10; // Average over iter iterations.
+    println!("Performance measurements:"); // Testing the performances on iter iterations.
+    let iter = 10;
     measurementscc2(&mut csprng, g0, iter);					
 }
