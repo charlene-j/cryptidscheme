0a8248f7c8ef57f2f246415efdb05a943318f79b
charlene-j <148769967+charlene-j@users.noreply.github.com>
2024-02-27 10:30:05 +0100

    Update main.rs

--- scheme1/src/main.rs
+++ scheme1/src/main.rs
@@ -8,7 +8,7 @@ fn main(){
     let g0 = RISTRETTO_BASEPOINT_POINT; // g0 is the generator used to build the public key of a player.
     let a = vec![RistrettoPoint::identity(), RISTRETTO_BASEPOINT_POINT]; // a is the encoding vector of true and false in RistrettoPoint.
     let n = 108;
-    println!("Performance measurements:"); // Test of performances on iter iterations.    
+    println!("Performance measurements:"); // Testing the performances on iter iterations.    
     let iter = 10;
     measurementscc1(&mut csprng, g0, (&a).to_vec(), n, iter);     
 } 
